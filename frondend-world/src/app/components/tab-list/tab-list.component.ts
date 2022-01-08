92e9a7875f153141e5dd8b864a72c9f2d2832d09
Nhật Nguyễn <ngquocnhat9317@gmail.com>
2022-01-08 21:56:45 +0700

    add tab info(1)

--- frondend-world/src/app/components/tab-list/tab-list.component.ts
+++ frondend-world/src/app/components/tab-list/tab-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { People } from 'src/app/interfaces/people';
+import { Person } from 'src/app/interfaces/person';
+import { Resource } from 'src/app/interfaces/resource';
+import { Terrain } from 'src/app/interfaces/terrain';
 import { CellInformationService } from 'src/app/services/cell-information.service';
 
 @Component({
@@ -11,7 +14,13 @@ export class TabListComponent implements OnInit, OnChanges {
 
     @Input() items?: string[];
     @Input() local?: string;
-    selected?: string
+    selected?: string;
+    personId?: number;
+
+    peopleInfo?: People;
+    personInfo?: Person;
+    terrainInfo?: Terrain;
+    resourceInfo?: Resource;
 
     constructor(private cellService: CellInformationService) {}
 
@@ -19,46 +28,74 @@ export class TabListComponent implements OnInit, OnChanges {
         this.chooseDefaultTab()
     }
 
-    chooseDefaultTab() {
-        if ((this.items != undefined && this.items.length > 0)
-                && (this.selected != undefined && !this.items.includes(this.selected))) {
-            this.selected = this.items[0]
-        }
-    }
-
     ngOnChanges(changes: SimpleChanges) {
         if(changes['items'] != undefined
                 && changes['items'].currentValue !== changes['items'].previousValue) {
             this.chooseDefaultTab()
         }
+        this.getInfo()
+
     }
 
-    onSelect(item: string) {
-        this.selected = item;
-    }
-
-    getInfo() {
-        if (this.selected === 'People') {
-        }
-        if (this.selected === 'Person') {
-        }
-        if (this.selected === 'Terrain') {
-        }
-        if (this.selected === 'Resource') {
+    chooseDefaultTab(): void {
+        if ((this.items != undefined && this.selected == undefined && this.items.length > 0)
+            || (this.items != undefined && this.selected != undefined && !this.items.includes(this.selected))) {
+            this.selected = this.items[0]
         }
     }
 
-    getPeopleInfo(): People {}
-
-    getPersonInfo() {
-
+    onSelectTab(selected: string): void {
+        this.selected = selected;
+        this.getInfo()
     }
 
-    getTerrainInfo() {
-
+    onSelectPerson(selected: number): void {
+        this.personId = selected;
     }
 
-    getResourceInfo() {
+    getInfo(): void {
+        if (this.local != undefined) {
+            if (this.selected === 'People') {
+                this.getPeopleInfo()
+            }
+            if (this.selected === 'Person') {
+                this.getPersonInfo()
+            }
+            if (this.selected === 'Terrain') {
+                this.getTerrainInfo()
+            }
+            if (this.selected === 'Resource') {
+                this.getResourceInfo()
+            }
+        }
+    }
 
+    getPeopleInfo(): void {
+        this.cellService.getPeopleInfo(this.local)
+            .subscribe(info => {
+                this.peopleInfo = info
+            })
+    }
+
+    getPersonInfo(): void {
+        if (this.personId !== undefined)
+            this.cellService.getPersonInfo(this.local, this.personId)
+                .subscribe(info => {
+                    this.personInfo = info
+                })
+    }
+
+    getTerrainInfo(): void {
+        this.cellService.getTerrainInfo(this.local)
+            .subscribe(info => {
+                this.terrainInfo = info
+            })
+    }
+
+    getResourceInfo(): void {
+        this.cellService.getResourceInfo(this.local)
+            .subscribe(info => {
+                this.resourceInfo = info
+            })
     }
 }
--- frondend-world/src/app/services/cell-information.service.ts
+++ frondend-world/src/app/services/cell-information.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { People } from 'src/app/interfaces/people';
 import { Person } from 'src/app/interfaces/person';
+import { Resource } from '../interfaces/resource';
+import { Terrain } from '../interfaces/terrain';
 
 const getDumpPeopleInfo = (): People => {
     let result = {
@@ -49,24 +51,46 @@ const getDumpPeopleInfo = (): People => {
     return result;
 };
 
-const getDumpPersonInfo = (id: number): Person => {
-    let result = {
-        'id': id,
-        'name': 'Victor',
-        'gender': 'male',
-        'age': 20,
-        'status': 'Hungry',
-        'happy': 100,
-        'relationship': [
-            {
-                'id': id + 1,
-                'name': 'Rose',
-                'gender': 'female',
-                'age': 18,
-                'position': 'wife'
-            }
-        ]
+const getDumpPersonInfo = (id: number | undefined): Person | undefined => {
+    let result = undefined;
+    if (id) {
+        result = {
+            'id': id,
+            'name': 'Victor',
+            'gender': 'male',
+            'age': 20,
+            'status': 'Hungry',
+            'happy': 100,
+            'relationship': [
+                {
+                    'id': id + 1,
+                    'name': 'Rose',
+                    'gender': 'female',
+                    'age': 18,
+                    'position': 'wife'
+                }
+            ]
+        }
     }
+    return result;
+}
+
+const getDumpTerrainInfo = (): Terrain => {
+    let result = {
+        'terrain': 'Forest',
+        'extremeLevel': 1
+    };
+    return result;
+}
+
+const getDumpResourceInfo = (): Resource => {
+    let result = {
+        'food': 100,
+        'water': 100,
+        'lumber': 100,
+        'rock': 100,
+        'metal': 100
+    };
     return result;
 }
 
@@ -77,13 +101,23 @@ export class CellInformationService {
 
     constructor() {}
 
-    getPeopleInfo(local: string): Observable<People> {
-        const peopleInfo = of(getDumpPeopleInfo());
+    getPeopleInfo(local: string | undefined): Observable<People> {
+        let peopleInfo = of(getDumpPeopleInfo());
         return peopleInfo;
     }
 
-    getPersonInfo(local: string, id: number): Observable<Person> {
-        const personInfo = of(getDumpPersonInfo(id));
+    getPersonInfo(local: string | undefined, id: number | undefined): Observable<Person | undefined> {
+        let personInfo = of(getDumpPersonInfo(id));
         return personInfo;
+    }
+
+    getTerrainInfo(local: string | undefined): Observable<Terrain> {
+        let terrainInfo = of(getDumpTerrainInfo());
+        return terrainInfo;
+    }
+
+    getResourceInfo(local: string | undefined): Observable<Resource> {
+        let resourceInfo = of(getDumpResourceInfo());
+        return resourceInfo;
     }
 }
